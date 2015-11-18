e6e9494c83d3ad2fbb3447121813d30057231759
Valery Frolov <valeryf@wix.com>
2015-11-18 22:25:32 +0200

    add test

--- test/cloj/expand_test.clj
+++ test/cloj/expand_test.clj
@@ -3,7 +3,7 @@
             [cloj.ast :refer :all]
             [cloj.expand :refer :all]))
 
-(deftest expand-test
+(deftest test-non-expandable
   (testing "forms that should not be expanded"
     (is (= (ast "123") (expand (ast "123"))))
     (is (= (ast "123 456") (expand (ast "123 456"))))
@@ -12,4 +12,10 @@
     (is (= (ast "(if 1 2 3)") (expand (ast "(if 1 2 3)"))))
     (is (= (ast "(+ 1 2 3)") (expand (ast "(+ 1 2 3)"))))
     (is (= (ast "(lambda (x) x)") (expand (ast "(lambda (x) x)"))))
+    ))
+
+
+(deftest test-expandable
+  (testing "forms that should be expanded"
+    (is (= (expand (ast "(let ((x 1)) x)")) (ast "((lambda (x) x) 1)")))
     ))
